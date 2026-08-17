c9db07ad16d9b929b52807561b8c3f0cf6e90e43
Mohammad Malkawi <mohammadmalkawi681@gmail.com>
2026-08-17 21:30:04 +0300

    test

--- Pharmacy-Inventory-Dispensing-App/src/PharmacyInventoryDispensingSystem.Infrastructure/DependencyInjection.cs
+++ Pharmacy-Inventory-Dispensing-App/src/PharmacyInventoryDispensingSystem.Infrastructure/DependencyInjection.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using PharmacyInventoryDispensingSystem.Application.Common.Interfaces.Repositories;
+using PharmacyInventoryDispensingSystem.Infrastructure.Identity;
 using PharmacyInventoryDispensingSystem.Infrastructure.Persistence.Context;
 using PharmacyInventoryDispensingSystem.Infrastructure.Persistence.Repositories;
 using System.Text;
@@ -26,11 +28,41 @@ public static class DependencyInjection
         //Register UnitOfWork ==>:
         services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-        
+
+        //Register Identity:
+        services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+        {
+            //Password Rules:
+            options.Password.RequireDigit = true;
+            options.Password.RequireLowercase = true;
+            options.Password.RequireUppercase = true;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequiredLength = 8;
+
+            //Lockout settings :
+
+            options.Lockout.MaxFailedAccessAttempts = 5; //N=5
+            options.Lockout.AllowedForNewUsers = true;
+            //يستمر حظر 15 دقيقة:
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+
+            //User requirements :
+
+            //This means each email must be unique in the system:
+            options.User.RequireUniqueEmail = true;
+            options.SignIn.RequireConfirmedEmail = false;
 
 
 
-            return services;
+
+        })
+        .AddEntityFrameworkStores<AppDbContext>()
+        .AddDefaultTokenProviders();
+
+
+
+
+        return services;
     }
 }
 
---
+++ Pharmacy-Inventory-Dispensing-App/src/PharmacyInventoryDispensingSystem.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PharmacyInventoryDispensingSystem.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmacyInventoryDispensingSystem.Infrastructure.Identity
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public bool IsActive { get; set; } = true;
+
+    }
+}
