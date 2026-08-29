7e279aa15d3e547e9ab77f067b0a2928bfe0207c
Mohammad Malkawi <mohammadmalkawi681@gmail.com>
2026-08-29 21:13:04 +0300

    feat: add partitioned rate limiting policies

---
+++ Pharmacy-Inventory-Dispensing-App/src/PharmacyInventoryDispensingSystem.WebApi/RateLimiting/RateLimitPolicyNames.cs
@@ -0,0 +1,11 @@
+﻿namespace PharmacyInventoryDispensingSystem.WebApi.RateLimiting
+{
+    public static class RateLimitPolicyNames
+    {
+        public const string AnonymousAuth = nameof(AnonymousAuth);
+        public const string AuthenticatedAuth =
+            nameof(AuthenticatedAuth);
+        public const string PaginatedQuery =
+            nameof(PaginatedQuery);
+    }
+}
